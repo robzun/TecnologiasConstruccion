08e10271d659f36754df78b4acb2e3221045531a
robzun <163868536+robzun@users.noreply.github.com>
2024-11-06 15:09:16 -0600

    Delete Bloque 3 directory

--- Bloque 3/Adapter/Diego/Adapter/User.py
+++
@@ -1,8 +0,0 @@
-class User:
-    def __init__(self,user_id,name,email):
-        self.user_id = user_id
-        self.name = name
-        self.email = email
-
-    def __repr__(self):
-        return f'ID: {self.user_id} nombre: {self.name} email: {self.email}'
